ef66bfe5dd8f7408e71eab22e294432f4ff0e7a7
Simon Wächter <waechter.simon@gmail.com>
2017-09-01 19:41:56 +0200

    Move the server adapter to an own file

--- angularj-universal-application/src/main/angular/server/server.ts
+++ angularj-universal-application/src/main/angular/server/server.ts
@@ -1,34 +1,13 @@
-require('zone.js/dist/zone-node');
-require('reflect-metadata');
+import {RenderAdapter, RenderCallback} from "./serveradapter";
 
 const AppServerModuleNgFactory = require('./../dist-server/main.bundle').AppServerModuleNgFactory;
-
-import {renderModuleFactory} from "@angular/platform-server";
 
 export declare function registerRenderAdapter(renderadapter: RenderAdapter): void;
 
 export declare function receiveRenderedPage(uuid: string, html: string, error: any): void;
 
-export class RenderAdapter {
+const rendercallback: RenderCallback = (uuid: string, html: string, error: any) => {
+    receiveRenderedPage(uuid, html, error);
+};
 
-    private appservermodulengfactory: any;
-
-    private html: string;
-
-    constructor(appservermodulengfactory: any) {
-        this.appservermodulengfactory = appservermodulengfactory;
-        this.html = "<app-root></app-root>";
-    }
-
-    setHtml(html: string) {
-        this.html = html;
-    }
-
-    renderPage(uuid: string, uri: string) {
-        renderModuleFactory(this.appservermodulengfactory, {document: this.html, url: uri}).then(html => {
-            receiveRenderedPage(uuid, html, null);
-        });
-    }
-}
-
-registerRenderAdapter(new RenderAdapter(AppServerModuleNgFactory));
+registerRenderAdapter(new RenderAdapter(AppServerModuleNgFactory, rendercallback));
---
+++ angularj-universal-application/src/main/angular/server/serveradapter.ts
@@ -0,0 +1,31 @@
+require('zone.js/dist/zone-node');
+require('reflect-metadata');
+
+import {renderModuleFactory} from "@angular/platform-server";
+
+export type RenderCallback = (uuid: string, html: string, error: any) => void;
+
+export class RenderAdapter {
+
+    private appservermodulengfactory: any;
+
+    private callback: RenderCallback;
+
+    private html: string;
+
+    constructor(appservermodulengfactory: any, callback: RenderCallback) {
+        this.appservermodulengfactory = appservermodulengfactory;
+        this.callback = callback;
+        this.html = "<app-root></app-root>";
+    }
+
+    setHtml(html: string) {
+        this.html = html;
+    }
+
+    renderPage(uuid: string, uri: string) {
+        renderModuleFactory(this.appservermodulengfactory, {document: this.html, url: uri}).then(html => {
+            this.callback(uuid, html, null);
+        });
+    }
+}

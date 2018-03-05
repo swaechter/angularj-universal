e0bf66788c9cd1e9393fd3992cee431e08bb6bb1
Simon Wächter <waechter.simon@gmail.com>
2018-03-05 09:15:15 +0100

    Add a proxy for local development

---
+++ angularj-universal-example-spring-boot/src/main/java/ch/swaechter/angularjuniversal/example/springboot/configuration/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package ch.swaechter.angularjuniversal.example.springboot.configuration;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+    }
+}
